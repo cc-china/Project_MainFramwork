3a6893443a732e127a92c930412610e104aca920
lolo980 <com_caoyun@163.com>
2019-04-25 18:06:19 +0800

    增加一个自定义RadioButton，当图片和文字无法全部居中时使用

---
+++ app/src/main/java/com/grirms/crm/view/MyRadioButton.java
@@ -0,0 +1,42 @@
+package com.grirms.crm.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.Gravity;
+
+/**
+ * Created by Administrator on 2019\4\25 0025.
+ * The purpose of This widget is to make it a perfect fit for RadioButton when drawableLeft
+ * and texts cannot be displayed in the middle
+ */
+
+public class MyRadioButton extends android.support.v7.widget.AppCompatRadioButton {
+
+    public MyRadioButton(Context context) {
+        super(context);
+    }
+
+    public MyRadioButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyRadioButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Drawable[] drawables = getCompoundDrawables();
+        Drawable drawable = drawables[0];
+        int gravity = getGravity();
+        int left = 0;
+        if (gravity == Gravity.CENTER) {
+            left = ((int) (getWidth() - drawable.getIntrinsicWidth() - getPaint().measureText(getText().toString())) / 2);
+        }
+        drawable.setBounds(left, 0, left + drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+    }
+
+}
